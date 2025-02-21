c5c58591b783ef68fef9fc2505d67be5c82e8dfd
AndreS2-12 <andres.agudelop@campusucc.edu.co>
2025-02-21 01:30:50 +0000

    Ejercicios Clase

---
+++ Clase3/CreacionArreglos.java
@@ -0,0 +1,15 @@
+package Clase3;
+public class CreacionArreglos {
+    public static void main(String[] args) {
+
+        // Creacion del arreglo de enteros
+
+        int [] a ={3,6,5,10,20,1,4,9};
+
+        //Rescorrer y mostrar los elementos del arreglo
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a["+ i +"]= " + a[i]);
+        }
+    }
+}
---
+++ Clase3/Ejercicio.java
@@ -0,0 +1,26 @@
+package Clase3;
+public class Ejercicio {
+   public static void main(String[] args) {
+        /*
+         * 3. dado un arreglo lineal de numeros, sumar separadamente
+         * los numeros pares y los impares.
+         * 
+         * Analisis: Declarar un arreglo, en dos variables guardar la suma
+         * de los pares y los impares, lo anterior debe iterar en un "for"
+         * con la condición de validar si el numero es par o impar (n%2==0 -> par)
+         */
+
+         int [] arreglo = {5, 2, 6, 5, 90, 9, 1, 8, 15, 4};
+         int sumaPares = 0, sumaImpares = 0;
+
+         for (int i = 0; i < arreglo.length; i++) {
+            if(arreglo[i]%2==0){
+            sumaPares += arreglo[i];
+         } else {
+            sumaImpares += arreglo[i];
+         }  
+      }
+      System.out.println("La suma de los pares es: " + sumaPares);
+      System.out.println("la suma de los impares es: " + sumaImpares);
+   }
+}
