abf2d14af3b9a01167bfa34cd236877414a8a266
AndreS2-12 <andres.agudelop@campusucc.edu.co>
2025-03-07 03:33:53 +0000

    Parte2

--- MiniProyecto/Principal.java
+++ MiniProyecto/Principal.java
@@ -1,47 +1,76 @@
 package MiniProyecto;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 
 public class Principal {
     public static void main(String[] args) {
         Moto bajocc[] = new Moto[20];
-       // Moto altocc[] = new Moto[10];
+        Moto altocc[] = new Moto[10];
 
+        int opc = -1;
         String placa;
         double cilindraje;
         String marca;
-    
-    /*    bajocc[1]=null;  
-    if (bajocc[1]==null) {
-        System.out.println("Está vacio...");
+
+        /*
+         * bajocc[1]=null;
+         * if (bajocc[1]==null) {
+         * System.out.println("Está vacio...");
+         * 
+         * }
+         */
+
+        for (int c = 0; c < 20; c++) {
+            bajocc[c] = null;
+        }
+        Scanner entrada = new Scanner(System.in);
         
-    }   */
-
-    for (int c = 0; c < 20; c++){
-        bajocc[c]=null;
-    }
-Scanner entrada = new Scanner(System.in);
-System.out.println("Por favor ingrese la placa: ");
-
-placa = entrada.next();
-
-System.out.println("Por favor ingrese el cilindraje: ");
-
-cilindraje = entrada.nextDouble();
-
-System.out.println("Por favor ingrese la marca: ");
-
-marca = entrada.next();
-
-bajocc[0] = new Moto(placa, cilindraje, marca);
-System.out.println(bajocc[0].getPlaca());
-
-/* placa = JOptionPane.showInputDialog("Por favor ingrese la placa: ");
-        cilindraje = Double.parseDouble(JOptionPane.showInputDialog("Por favor ingrese el cilindraje: "));
-        marca = JOptionPane.showInputDialog("Por favor ingrese la marca: ");
         bajocc[0] = new Moto(placa, cilindraje, marca);
         System.out.println(bajocc[0].getPlaca());
-    }*/
-        
-}
+
+
+
+            System.out.println("1. Registrar moto: ");
+            System.out.println("2. Calcular cobro: ");
+            System.out.println("3. Salir: ");
+
+        do{
+             
+            System.out.println("1. Registrar moto: ");
+            System.out.println("2. Calcular cobro: ");
+            System.out.println("3. Salir: ");
+            switch(opc){
+                case 1: {System.out.println("Por favor ingrese la placa: ");
+
+        placa = entrada.next();
+
+        System.out.println("Por favor ingrese el cilindraje: ");
+
+        cilindraje = entrada.nextDouble();
+
+        System.out.println("Por favor ingrese la marca: ");
+
+        marca = entrada.next();
+}            
+               
+                case 2: {}
+
+                case 3: {}
+
+                default: {System.out.println("Está opción no es valida...");} break;
+            
+            int opcion = Scanner.nextInt();
+            if (opcion == 1) {
+                System.out.println("Ingrese la placa: ");
+                String placa = Scanner.nextLine;
+
+                System.out.println("Ingrese el cilindraje: ");
+                double cilindraje = Scanner.nextLine;
+
+                System.out.println("Ingrese la marca: ");
+                String marca = Scanner.nextLine;
+            
+            }
+        }while ();
+
+    }
 }
